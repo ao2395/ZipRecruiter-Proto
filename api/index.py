f4748e85ae10c20e1f50c79e227fb04fabe47ea7
ao2395 <ao2395@nyu.edu>
2026-02-04 19:34:39 +0400

    Use Vercel ASGI format

--- api/index.py
+++ api/index.py
@@ -1,3 +1,6 @@
 from .main import app
+from starlette.types import ASGIApp, Receive, Scope, Send
 
-# Vercel will automatically detect and wrap the ASGI app
+# Export app for Vercel's ASGI support
+# Vercel recognizes 'app' as the ASGI application
+__all__ = ['app']
