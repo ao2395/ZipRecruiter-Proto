f72ec52027c3a41df6b864771be2518a56573788
ao2395 <ao2395@nyu.edu>
2026-02-04 19:32:04 +0400

    Simplify - let Vercel handle ASGI natively

--- api/index.py
+++ api/index.py
@@ -1,14 +1,3 @@
-"""
-Vercel Serverless Function Entry Point
-"""
 from .main import app
 
-# For Vercel, we need to export a handler function
-# Vercel's Python runtime will call this
-def handler(event, context):
-    """
-    AWS Lambda handler format that Vercel expects
-    """
-    from mangum import Mangum
-    asgi_handler = Mangum(app, lifespan="off")
-    return asgi_handler(event, context)
+# Vercel will automatically detect and wrap the ASGI app
